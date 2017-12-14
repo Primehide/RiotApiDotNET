580333429637fea204a0a83631bdb27713cd05d3
Ironhide <primeservicespp@gmail.com>
2017-12-14 11:47:39 +0100

    SUMMONER-V3 implementation Test client

--- RiotApiDotNET/RiotClient.cs
+++ RiotApiDotNET/RiotClient.cs
@@ -19,14 +19,27 @@ namespace RiotApiDotNET
             Server = server;
     }
 
-        private async Task<SUMMONER_V3.SummonerDTO> SendRequest()
+        private async Task<SUMMONER_V3.SummonerDTO> SendRequest(int WhichMethod,string Summonername = "", long AccountId = 0, long SummonerId = 0)
         {
-            string baseUriSummoner = "https://" + Server.ToString() + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/";
+            string baseUri = "https://" + Server.ToString() + ".api.riotgames.com/lol/summoner/v3/summoners/";
+
+            if(WhichMethod == 1)
+            {
+                baseUri = baseUri + "/by-name/" + Summonername + "?api_key=" + Apikey;
+            }
+            else if(WhichMethod == 2)
+            {
+                baseUri = baseUri + "/by-account/" + AccountId + "?api_key=" + Apikey;
+            }
+            else
+            {
+                baseUri = baseUri + SummonerId + "?api_key=" + Apikey;
+            }
 
 
             using (HttpClient http = new HttpClient())
             {
-                string uri = baseUriSummoner;
+                string uri = baseUri;
                 HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Get, uri);
                 var response = await http.GetAsync(uri);
                 var jsonString = await response.Content.ReadAsStringAsync();
@@ -35,11 +48,19 @@ namespace RiotApiDotNET
         }
 
         //SUMMONER-V3 Methods
-        /*
-        public SUMMONER_V3.SummonerDTO GetSummonerById(long accountid)
+        public SUMMONER_V3.SummonerDTO GetSummonerByName(string Summonername)
         {
-
+            return SendRequest(1,Summonername: Summonername).Result;
         }
-        */
+
+        public SUMMONER_V3.SummonerDTO GetSummonerByAccountId(long AccountId)
+        {
+            return SendRequest(2,AccountId: AccountId).Result;
+        }
+
+        public SUMMONER_V3.SummonerDTO GetSummonerBySummonerId(long SummonerId)
+        {
+            return SendRequest(3,SummonerId: SummonerId).Result;
+        }
     }
 }
---
+++ RiotTestClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RiotApiDotNET;
+
+namespace RiotTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RiotClient client = new RiotClient("RGAPI-7dbd7ac3-2c1d-4d04-a9f2-8be56256711a", RiotApiDotNET.Enums.Server.EUW1);
+            RiotApiDotNET.SUMMONER_V3.SummonerDTO summoner = client.GetSummonerBySummonerId(28041965);
+            Console.WriteLine(summoner.name);
+            Console.ReadLine();
+        }
+    }
+}
